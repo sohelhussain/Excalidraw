73ac86f408fe1f0d3f7c39dc0de1ad94dea9f3bc
sohelhussain <sohelhussaing@gmail.com>
2025-01-22 18:48:56 +0530

    feat: CREATING A STRUCTURE

--- apps/web/app/canvas/[roomName]/page.tsx
+++ apps/web/app/canvas/[roomName]/page.tsx
@@ -16,7 +16,20 @@ export default function Canvas() {
         }
     }, [canvasRef])
 
-    return <div className="w-full h-screen">
-        <canvas ref={canvasRef} ></canvas>
+    return <div className="w-full h-screen relative">
+        <canvas ref={canvasRef}></canvas>
+        <div className="absolute top-0 left-0 z-10 w-full h-full pointer-events-none">
+            <nav className="w-full h-[5%] text-white flex justify-center items-center">
+                selector
+            </nav>
+            <div className="w-full h-[95%] pointer-events-none flex">
+                <div className="w-[10%] h-full text-white">
+                    adjuture
+                </div>
+                <div className="w-full h-full text-white pointer-events-none">
+                    message
+                </div>
+            </div>
+        </div>
     </div>
 }
